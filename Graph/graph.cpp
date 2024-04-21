c725a63d932d78b10ad5defbe957b802e6bdb8dc
AyushBadola22 <ayush.badola.04@gmail.com>
2024-04-21 10:55:42 +0530

    ShotestPath  and pathExists or not
    Shortest path in unweighted graph using BFS traversal .
    Path exists using dfs .
    
    Other things are in progress

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -1,6 +1,7 @@
 #include<iostream> 
 #include<vector>
 #include<queue>
+#include<algorithm>
 using namespace std; 
 
 /**
@@ -53,12 +54,77 @@ void addEdge (vector<vector<int>> & graph , int source , int destination){
     graph[destination].emplace_back(source); 
 }
 
+void addEdge (vector<vector<int>> & graph , int source , vector<int> destinations){
+    for(auto destination : destinations){
+        graph[destination].push_back(source); 
+        graph[source].push_back(destination); 
+    }
+}
+
+void displayAdjacency (vector<vector <int>> graph){
+    for(int i =0; i<graph.size(); i++){
+        cout<<i<<" -> "; 
+        for (int j = 0; j<graph[i].size() ; j++){
+            cout<<graph[i][j]<<" "; 
+        }
+        cout<<endl; 
+    }
+    cout<<endl; 
+}
+
+vector<int> shortestPathBFS(vector<vector<int>> &graph , int source ){
+    queue<pair<int , int>> q; 
+    vector<int> distance (graph.size(), INT_MAX); 
+    distance[source] = 0; 
+    q.emplace(pair<int , int>{source , distance[source]});
+
+    while(!q.empty()){
+        auto curVertex = q.front(); 
+        q.pop(); 
+        int weight = curVertex.second + 1 ; 
+        for(auto edge : graph[curVertex.first]){
+            if(distance[edge] > weight){
+                q.emplace(pair<int , int> {edge , weight});
+                distance[edge] = weight; 
+            }
+        }
+    } 
+    return distance; 
+}
+
+bool findPathDFS (vector<vector<int>> graph , int source , int destination , vector<bool>&visited){
+    if(visited.empty()){
+        visited.resize(graph.size());
+        fill(visited.begin() , visited.end(), false);    
+    }
+    if(source == destination) return true; 
+    if(visited[source]) return false; 
+
+    visited[source] = true; 
+
+    for(auto vertex : graph[source]){
+        if(findPathDFS(graph, vertex , destination , visited)) return true; 
+    }
+    return false; 
+}
+bool findPath(vector<vector<int>> graph , int source , int destination ){
+    vector<bool> visited (graph.size() , false); 
+    return findPathDFS(graph , source , destination, visited); 
+}
+
+
+// **************************** Disjoint Structure ****************************
+
+
 int main(){
-    int numberOfVertices = 5; 
+    int numberOfVertices = 8; 
     vector<vector<int>> graph;
     graph.resize(numberOfVertices);
-    addEdge(graph, 0, 1 );
-    addEdge(graph, 1, 2 );
-    addEdge(graph, 3, 4 );
-    DFS(graph ); 
+    addEdge (graph, 0 , {1, 2}); 
+    addEdge (graph, 1, 3); 
+    addEdge (graph, 2, 4); 
+    addEdge (graph, 4, 6); 
+    addEdge (graph, 6, 7); 
+    addEdge (graph, 7, 5);
+
 }
